514def0a81cf8cc5b70c616892847ee5f376caaa
DAkyl <DastanAkyl01@gmail.com>
2020-08-16 13:41:49 -0700

    Coderbyte assessment

---
+++ src/test/java/coderByte/AssessmentSolutions.java
@@ -0,0 +1,38 @@
+package coderByte;
+
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.*;
+
+public class AssessmentSolutions {
+
+    private final String URL = "https://api.football-data.org/v2/teams/12";
+    private final String token = "e051043b86624518a57a263f9388d198";
+
+    @Test
+    public void getStatusCode200() {
+
+        given().header("Content-Type", "application/json").header("X-Auth-Token", token)
+                .when().get(URL).then().assertThat().statusCode(200).extract().response();
+    }
+
+    @Test
+    public void getStatusCode403() {
+
+        given().header("Content-Type", "application/json")
+                .when().get(URL).then().assertThat().statusCode(403).extract().response();
+    }
+
+    @Test
+    public void squadNumebr() {
+
+        Response response = given().header("Content-Type", "application/json").header("X-Auth-Token", token)
+                .when().get(URL).then().assertThat().statusCode(200).extract().response();
+        JsonPath js = new JsonPath(response.asString());
+        int count = js.getInt("squad.size()");
+        Assert.assertTrue(count > 20);
+    }
+}
