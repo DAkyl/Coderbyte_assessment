f07520bbd175c6ecdeefdf0bd6d92b848d584213
DAkyl <69769658+DAkyl@users.noreply.github.com>
2020-08-16 14:14:59 -0700

    Update AssessmentSolutions.java

--- src/test/java/coderByte/AssessmentSolutions.java
+++ src/test/java/coderByte/AssessmentSolutions.java
@@ -11,21 +11,27 @@ public class AssessmentSolutions {
 
     private final String URL = "https://api.football-data.org/v2/teams/12";
     private final String token = "e051043b86624518a57a263f9388d198";
-
+    /**
+        Validate 200 http code on successful response
+     **/
     @Test
     public void getStatusCode200() {
 
         given().header("Content-Type", "application/json").header("X-Auth-Token", token)
                 .when().get(URL).then().assertThat().statusCode(200).extract().response();
     }
-
+    /**
+        Validate 403 http code on failed authentication
+     **/
     @Test
     public void getStatusCode403() {
 
         given().header("Content-Type", "application/json")
                 .when().get(URL).then().assertThat().statusCode(403).extract().response();
     }
-
+    /**
+     Validate more than 20 squad members are there in API response
+     **/
     @Test
     public void squadNumebr() {
 
